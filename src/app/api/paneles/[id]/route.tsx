78797fef863963c55e9952cbe3c29cd27296cf4c
Arvedson <tomasarvedson@gmail.com>
2024-07-11 11:15:42 -0600

    deploy test0.5

--- src/app/api/paneles/[id]/route.tsx
+++ src/app/api/paneles/[id]/route.tsx
@@ -2,11 +2,11 @@ import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '../../../../../lib/prisma';
 
 // GET handler for a single panel
-export async function GET(req: NextRequest, { params }) {
+export const GET = async (req: NextRequest, { params }) => {
   const { id } = params;
 
   if (!id) {
-    return NextResponse.json({ error: 'ID is required' }, { status: 400 });
+    return new NextResponse(JSON.stringify({ error: 'ID is required' }), { status: 400 });
   }
 
   try {
@@ -15,23 +15,23 @@ export async function GET(req: NextRequest, { params }) {
     });
 
     if (panel) {
-      return NextResponse.json(panel, { status: 200 });
+      return new NextResponse(JSON.stringify(panel), { status: 200 });
     } else {
-      return NextResponse.json({ error: 'Panel not found' }, { status: 404 });
+      return new NextResponse(JSON.stringify({ error: 'Panel not found' }), { status: 404 });
     }
   } catch (error) {
     console.error('Error fetching panel', error);
-    return NextResponse.json({ error: 'Error fetching panel' }, { status: 500 });
+    return new NextResponse(JSON.stringify({ error: 'Error fetching panel' }), { status: 500 });
   }
-}
+};
 
 // PUT handler for updating a single panel
-export async function PUT(req: NextRequest, { params }) {
+export const PUT = async (req: NextRequest, { params }) => {
   const { id } = params;
   const data = await req.json();
 
   if (!id) {
-    return NextResponse.json({ error: 'ID is required' }, { status: 400 });
+    return new NextResponse(JSON.stringify({ error: 'ID is required' }), { status: 400 });
   }
 
   try {
@@ -60,19 +60,19 @@ export async function PUT(req: NextRequest, { params }) {
       },
     });
 
-    return NextResponse.json(updatedPanel, { status: 200 });
+    return new NextResponse(JSON.stringify(updatedPanel), { status: 200 });
   } catch (error) {
     console.error('Error updating panel', error);
-    return NextResponse.json({ error: 'Error updating panel' }, { status: 500 });
+    return new NextResponse(JSON.stringify({ error: 'Error updating panel' }), { status: 500 });
   }
-}
+};
 
 // DELETE handler for deleting a single panel
-export async function DELETE(req: NextRequest, { params }) {
+export const DELETE = async (req: NextRequest, { params }) => {
   const { id } = params;
 
   if (!id) {
-    return NextResponse.json({ error: 'ID is required' }, { status: 400 });
+    return new NextResponse(JSON.stringify({ error: 'ID is required' }), { status: 400 });
   }
 
   try {
@@ -80,9 +80,9 @@ export async function DELETE(req: NextRequest, { params }) {
       where: { id: Number(id) },
     });
 
-    return NextResponse.json({ message: 'Panel deleted successfully' }, { status: 200 });
+    return new NextResponse(JSON.stringify({ message: 'Panel deleted successfully' }), { status: 200 });
   } catch (error) {
     console.error('Error deleting panel', error);
-    return NextResponse.json({ error: 'Error deleting panel' }, { status: 500 });
+    return new NextResponse(JSON.stringify({ error: 'Error deleting panel' }), { status: 500 });
   }
-}
+};
--- src/app/api/paneles/route.tsx
+++ src/app/api/paneles/route.tsx
@@ -1,9 +1,8 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '../../../../lib/prisma';
-import { NextResponse } from 'next/server';
 
 // GET handler
-export async function GET(req: NextApiRequest) {
+export const GET = async (req: NextRequest) => {
   console.log('GET request received');
   try {
     console.log('Fetching panels from the database');
@@ -11,31 +10,95 @@ export async function GET(req: NextApiRequest) {
     const totalCount = await prisma.panelSolar.count();
     console.log('Panels fetched successfully', paneles);
 
-    const headers = {
+    const headers = new Headers({
       'Content-Range': `paneles 0-${paneles.length}/${totalCount}`,
-    };
+      'Access-Control-Expose-Headers': 'Content-Range',
+    });
 
-    return NextResponse.json(paneles, { status: 200, headers });
+    return new NextResponse(JSON.stringify(paneles), {
+      status: 200,
+      headers,
+    });
   } catch (error) {
     console.error('Error fetching panels', error);
-    return NextResponse.json({ error: 'Error fetching panels' }, { status: 500 });
+    return new NextResponse(JSON.stringify({ error: 'Error fetching panels' }), { status: 500 });
   }
-}
+};
 
 // POST handler
-export async function POST(req: NextApiRequest) {
+export const POST = async (req: NextRequest) => {
   const data = await req.json();
-  const { modelo, capacidadW, fabricante, eficiencia, dimensiones, peso, precio, imageUrl, voltajeVoc, corrienteIsc, voltajeVmp, corrienteImp, tipoCelda, cantidadCeldas, tolerancia, maxVoltajeSistema, temperaturaOperacion, garantiaProducto, garantiaPotencia } = data;
-  console.log('POST request received with data:', { modelo, capacidadW, fabricante, eficiencia, dimensiones, peso, precio, imageUrl, voltajeVoc, corrienteIsc, voltajeVmp, corrienteImp, tipoCelda, cantidadCeldas, tolerancia, maxVoltajeSistema, temperaturaOperacion, garantiaProducto, garantiaPotencia });
+  const {
+    modelo,
+    capacidadW,
+    fabricante,
+    eficiencia,
+    dimensiones,
+    peso,
+    precio,
+    imageUrl,
+    voltajeVoc,
+    corrienteIsc,
+    voltajeVmp,
+    corrienteImp,
+    tipoCelda,
+    cantidadCeldas,
+    tolerancia,
+    maxVoltajeSistema,
+    temperaturaOperacion,
+    garantiaProducto,
+    garantiaPotencia,
+  } = data;
+  console.log('POST request received with data:', {
+    modelo,
+    capacidadW,
+    fabricante,
+    eficiencia,
+    dimensiones,
+    peso,
+    precio,
+    imageUrl,
+    voltajeVoc,
+    corrienteIsc,
+    voltajeVmp,
+    corrienteImp,
+    tipoCelda,
+    cantidadCeldas,
+    tolerancia,
+    maxVoltajeSistema,
+    temperaturaOperacion,
+    garantiaProducto,
+    garantiaPotencia,
+  });
   try {
     console.log('Creating new panel in the database');
     const newPanel = await prisma.panelSolar.create({
-      data: { modelo, capacidadW, fabricante, eficiencia, dimensiones, peso, precio, imageUrl, voltajeVoc, corrienteIsc, voltajeVmp, corrienteImp, tipoCelda, cantidadCeldas, tolerancia, maxVoltajeSistema, temperaturaOperacion, garantiaProducto, garantiaPotencia }
+      data: {
+        modelo,
+        capacidadW,
+        fabricante,
+        eficiencia,
+        dimensiones,
+        peso,
+        precio,
+        imageUrl,
+        voltajeVoc,
+        corrienteIsc,
+        voltajeVmp,
+        corrienteImp,
+        tipoCelda,
+        cantidadCeldas,
+        tolerancia,
+        maxVoltajeSistema,
+        temperaturaOperacion,
+        garantiaProducto,
+        garantiaPotencia,
+      },
     });
     console.log('Panel created successfully', newPanel);
-    return NextResponse.json(newPanel, { status: 201 });
+    return new NextResponse(JSON.stringify(newPanel), { status: 201 });
   } catch (error) {
     console.error('Error creating panel', error);
-    return NextResponse.json({ error: 'Error creating panel' }, { status: 500 });
+    return new NextResponse(JSON.stringify({ error: 'Error creating panel' }), { status: 500 });
   }
-}
+};
