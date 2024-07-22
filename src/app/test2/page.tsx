c7a953e9123048d73ea982572f102728a8dce5b0
Arvedson <tomasarvedson@gmail.com>
2024-07-22 13:46:15 -0600

    deployfinal0.2

---
+++ src/app/test2/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import TChart from "../../components/Tchart";
+
+
+const TestPage: React.FC = () => {
+    return (
+      <div>
+        <TChart/>
+      </div>
+     
+
+
+    
+
+  
+  
+    );
+};
+
+export default TestPage;
